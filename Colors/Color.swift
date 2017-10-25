3c12d6e57b5d95f3e283b9dd8baf8eae7d00830a
Aidan Madden <aidanmadden@mwc-029062.dhcp.missouri.edu>
2017-10-25 14:25:52 -0500

    added structs and cell background colors

---
+++ Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors with Navigation
+//
+//  Created by Aidan Madden on 10/25/17.
+//  Copyright © 2017 Aidan Madden. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
